c3af3f621e9e06de8ee840310af1155041499989
largetimmo <kylechen49@gmail.cvom>
2017-10-17 20:54:57 -0400

    Update database

--- database_query.sql
+++ database_query.sql
@@ -1,50 +1,90 @@
-create table carpool
+create table BLOCKED_IP
 (
-	id int auto_increment
-		primary key,
-	uid int not null,
-	date date not null,
-	price int not null,
-	capacity int not null,
-	departure varchar(30) not null,
-	destination varchar(30) not null
+	ipaddr varchar(15) not null
+		primary key
 )
 ;
 
-create table carpool_book
+create table BOOKED_CARPOOL
 (
 	booking_ref int auto_increment
 		primary key,
 	uid int not null,
-	id int not null
+	id int not null,
+	seat int not null
 )
 ;
 
-create table city
+create index carpool_book_carpool_id_fk
+	on BOOKED_CARPOOL (id)
+;
+
+create index carpool_book_user_reg_info_uid_fk
+	on BOOKED_CARPOOL (uid)
+;
+
+create table CARPOOL
+(
+	id int auto_increment
+		primary key,
+	uid int not null,
+	date datetime not null,
+	price int not null,
+	capacity int not null,
+	departure varchar(30) not null,
+	destination varchar(30) not null,
+	remainseat int not null
+)
+;
+
+create index carpool_user_reg_info_uid_fk
+	on CARPOOL (uid)
+;
+
+alter table BOOKED_CARPOOL
+	add constraint BOOKED_CARPOOL_CARPOOL_id_fk
+		foreign key (id) references CARPOOL (id)
+;
+
+create table SUPPORTCITY
 (
 	city varchar(30) not null
 		primary key
 )
 ;
 
-create table user_carpool_info
+create table USER_REG
 (
 	uid int auto_increment
 		primary key,
 	username varchar(30) not null,
-	userlevel int not null,
-	vehicle varchar(30) not null,
-	exp int not null
+	password varchar(32) not null,
+	nickname varchar(30) not null,
+	email varchar(32) not null,
+	cell varchar(10) not null
 )
 ;
 
-create table user_reg_info
+alter table BOOKED_CARPOOL
+	add constraint BOOKED_CARPOOL_USER_REG_uid_fk
+		foreign key (uid) references USER_REG (uid)
+;
+
+alter table CARPOOL
+	add constraint CARPOOL_USER_REG_uid_fk
+		foreign key (uid) references USER_REG (uid)
+;
+
+create table VEHICLE_OWNER
 (
-	uid int not null
+	uid int auto_increment
 		primary key,
-	username varchar(30) not null,
-	password varchar(32) not null,
-	nickname varchar(30) not null
+	userlevel int not null,
+	vehicle varchar(30) not null,
+	exp int not null,
+	constraint VEHICLE_OWNER_USER_REG_uid_fk
+		foreign key (uid) references USER_REG (uid)
 )
 ;
+
 
