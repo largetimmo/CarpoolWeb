5f7a97202a696742609deb3fe140bdacec8a14fe
largetimmo <kylechen49@gmail.cvom>
2017-10-17 20:53:02 -0400

    Add Singleton

--- src/dao/IPAddrDAO.java
+++ src/dao/IPAddrDAO.java
@@ -6,9 +6,15 @@ import java.sql.*;
  * Created by admin on 2017/8/22.
  */
 public class IPAddrDAO extends AbstractDAO {
-    public static boolean checkBlockIP(String ipaddr) throws ClassNotFoundException, SQLException {
-        Class.forName(MYSQL_CLASS_NAME);
-        Connection connection = DriverManager.getConnection(ADDR,USERNAME,PASSWORD);
+    private IPAddrDAO(){
+
+    }
+    private static IPAddrDAO instance = new IPAddrDAO();
+    public static IPAddrDAO getInstance(){
+        return instance;
+    }
+    public boolean checkBlockIP(String ipaddr) throws SQLException {
+        Connection connection = ConnectionPool.getInstance().getGeneralConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM blocked_ip WHERE ipaddr = ?");
         preparedStatement.setString(1,ipaddr);
         ResultSet resultSet = preparedStatement.executeQuery();
