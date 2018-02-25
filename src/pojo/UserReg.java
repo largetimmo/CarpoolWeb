caea160224fd0bd55b4828c3da2eabccc6b3b3e9
largetimmo <kylechen49@gmail.com>
2018-02-25 15:28:11 +0800

    Combine two classes

--- src/pojo/UserReg.java
+++ src/pojo/UserReg.java
@@ -1,14 +1,18 @@
 package pojo;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.Generated;
+
+import javax.persistence.*;
 
 /**
  * Created by admin on 2017/8/15.
  */
 @Entity
 @Table(name = "USER_REG")
-public class UserReg extends UserInfo{
+public class UserReg{
+    private int uid;
+    private String username;
+    private String password;
     private String email;
     private String nickname;
     private String cell;
@@ -25,17 +29,55 @@ public class UserReg extends UserInfo{
         this.nickname = nickname;
         this.cell = cell;
     }
-
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+    @Column(name = "uid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    public int getUid() {
+        return uid;
+    }
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+    @Column(name = "email")
     public String getEmail() {
         return email;
     }
-
+    @Column(name = "nickname")
     public String getNickname() {
         return nickname;
     }
-
+    @Column(name = "cell")
     public String getCell() {
         return cell;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setCell(String cell) {
+        this.cell = cell;
     }
 
     @Override
@@ -48,4 +90,5 @@ public class UserReg extends UserInfo{
                 ", password='" + password + '\'' +
                 '}';
     }
+
 }
